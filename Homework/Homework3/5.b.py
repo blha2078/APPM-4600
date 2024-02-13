822468291f3d706959c5d3b0d38c00fdd9e52dd5
Blake Hamilton <blha2078@colorado.edu>
2024-02-13 13:16:43 -0700

    created 5.b

---
+++ Homework/Homework3/5.b.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def fixedpt(f,x0,tol,Nmax):
+
+    ''' x0 = initial guess''' 
+    ''' Nmax = max number of iterations'''
+    ''' tol = stopping tolerance'''
+
+    count = 0
+    while (count <Nmax):
+       count = count +1
+       x1 = f(x0)
+       if (abs(x1-x0) <tol):
+          xstar = x1
+          ier = 0
+          return [xstar,ier]
+       x0 = x1
+
+    xstar = x1
+    ier = 1
+    return [xstar, ier]
